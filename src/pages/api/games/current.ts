f48baff4f8dd8358336bb60866ae666626ce2b74
Matt Ruedinger <mruedinger@mattbook.local>
2026-01-14 21:36:27 -0500

    Allow setting time to beat in game modal

--- src/pages/api/games/current.ts
+++ src/pages/api/games/current.ts
@@ -22,6 +22,7 @@ type GameRow = {
 	best_price_cents?: number;
 	steam_app_id?: number;
 	itad_slug?: string;
+	time_to_beat_minutes?: number;
 };
 
 export const prerender = false;
@@ -35,7 +36,7 @@ export const GET: APIRoute = async ({ locals }) => {
 
 	const current = await db
 		.prepare(
-			"select games.id, games.title, games.submitted_by_email, members.name as submitted_by_name, members.alias as submitted_by_alias, games.cover_art_url, games.tags_json, games.description, games.current_price_cents, games.best_price_cents, games.steam_app_id, games.itad_slug " +
+			"select games.id, games.title, games.submitted_by_email, members.name as submitted_by_name, members.alias as submitted_by_alias, games.cover_art_url, games.tags_json, games.description, games.current_price_cents, games.best_price_cents, games.steam_app_id, games.itad_slug, games.time_to_beat_minutes " +
 				"from games left join members on members.email = games.submitted_by_email where games.status = 'current' limit 1"
 		)
 		.bind()
